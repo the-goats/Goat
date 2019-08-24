ae72fd5a4111fe9ec177034de28d30d8ed21f1ba
Stef Spakman <mail@stefspakman.com>
2019-08-24 14:55:46 +0200

    fractal

--- packages/goat-fractal/scripts/fractal.js
+++ packages/goat-fractal/scripts/fractal.js
@@ -1,19 +1,29 @@
 const {
   normalize
 } = require('path');
-const fractal = require('@frctl/fractal').create();
+const fractal = require('@frctl/fractal');
 const mandelbrot = require('@frctl/mandelbrot');
 const twigAdapter = require('@wondrousllc/fractal-twig-drupal-adapter');
 const { get } = require('lodash');
 const fs = require('fs');
 
-module.exports = ({
-  path,
-  configuration,
-  Notifier,
-}) => {
 
-  fractal.components.engine(twigAdapter({
+const createSymlink = (source, destination) => {
+  return fs.promises.symlink(source, destination);
+}
+
+const checkSymLinkExists = (source) => {
+  return new Promise(async (resolve) => {
+    try {
+      resolve(await fs.promises.readlink(source));
+    } catch {
+      resolve(false);
+    }
+  })
+}
+
+const fractalAddEngine = (styleguide) => {
+  styleguide.components.engine(twigAdapter({
     handlePrefix: '@components/',
     filters: {
       render(str) {
@@ -24,51 +34,80 @@ module.exports = ({
       }
     }
   }));
-  fractal.components.set('ext', '.twig');
+  styleguide.components.set('ext', '.twig');
+  return styleguide;
+}
 
+const fractalAddTheme = (configuration, currentPath, styleguide) => {
   const theme = mandelbrot({
     skin: get(configuration, 'configuration.fractal.theme.skin') || "navy",
     panels: get(configuration, 'configuration.fractal.theme.panels') || ["info", "html", "view", "context", "resources", "notes"],
-    styles: ['default', `/${path}/styleguide/theme/overrides.css`]
+    styles: ['default', normalize(`/${currentPath}/styleguide/theme/overrides.css`)]
   });
 
-  fractal.web.theme(theme);
+  styleguide.web.theme(theme);
+  return styleguide;
+}
 
-
-  /* Set the title of the project */
-  fractal.set('project.title', configuration.name);
+const fractalSetMeta = (configuration, styleguide) => {
+  styleguide.set('project.title', configuration.name);
   if (configuration.author) {
-    fractal.set('project.author', configuration.author);
+    styleguide.set('project.author', configuration.author);
+  }
+  return styleguide;
+}
+
+const fractalSetPaths = (configuration, currentPath, styleguide) => {
+  /* Tell Fractal where the components will live */
+  styleguide.components.set('path', normalize(`${currentPath}/components`));
+  /* Tell Fractal where the documentation pages will live */
+  styleguide.docs.set('path', normalize(`${currentPath}/docs`));
+  /* Specify a directory of static assets */
+  styleguide.web.set('static.path', normalize(`${currentPath}/.goat/temp/fractal/assets/`));
+  /* Set the static HTML build destination */
+  styleguide.web.set('builder.dest', normalize(`${currentPath}/.goat/temp/fractal/styleguide`));
+  return styleguide;
+}
+
+const fractalBuilder = (Notifier, styleguide) => {
+  const builder = styleguide.web.builder();
+  builder.on('progress', (completed, total) => Notifier.singleLine(`Exported ${completed} of ${total} items`, 'info'));
+  builder.on('error', err => Notifier.error(err.message));
+  return builder.build()
+}
+
+module.exports = async ({
+  path,
+  configuration,
+  Notifier,
+}) => {
+  const currentPath = normalize(`${path}`);
+  const styleGuideAssetsPath = normalize(`${path}/.goat/temp/fractal/assets`);
+
+  if (await checkSymLinkExists(styleGuideAssetsPath) !== currentPath) {
+    createSymlink(currentPath, styleGuideAssetsPath);
+    Notifier.log('Symlink created');
   }
 
-  fs.symlink(normalize(`${path}`), normalize(`${path}/.goat/temp/fractal/assets`), () => console.log('Symlink created'));
-  // fs.symlink(normalize(`${path}/css`), normalize(`${path}/.goat/temp/fractal/assets/css`), () => console.log('CSS symlink created'));
-  // fs.symlink(normalize(`${path}/test`), normalize(`${path}/.goat/temp/fractal/assets/test`), () => console.log('Test symlink created'));
-  // fs.symlink(normalize(`${path}/fonts`), normalize(`${path}/.goat/temp/fractal/assets/fonts`), () => console.log('fonts symlink created'));
+  let styleguide = fractal.create();
 
-  /* Tell Fractal where the components will live */
-  fractal.components.set('path', normalize(`${path}/components`));
-  /* Tell Fractal where the documentation pages will live */
-  fractal.docs.set('path', normalize(`${path}/docs`));
-  /* Specify a directory of static assets */
-  fractal.web.set('static.path', normalize(`${path}/.goat/temp/fractal/assets/`));
-  /* Set the static HTML build destination */
-  fractal.web.set('builder.dest', normalize(`${path}/.goat/temp/fractal/styleguide`));
+  styleguide = fractalAddEngine(styleguide);
+  styleguide = fractalAddTheme(configuration, currentPath, styleguide);
+  styleguide = fractalSetMeta(configuration, styleguide);
+  styleguide = fractalSetPaths(configuration, currentPath, styleguide);
 
-  const builder = fractal.web.builder();
-  const logger = fractal.cli.console;
-  builder.on('progress', (completed, total) => Notifier.singleLine(`Exported ${completed} of ${total} items`, 'info'));
-  builder.on('error', err => console.error(err.message));
-  builder.build().then(() => {
-    console.log('Fractal build completed!');
+  const build = fractalBuilder(Notifier, styleguide);
 
-    const server = fractal.web.server({
+  build.then(() => {
+    Notifier.log('Fractal build completed!');
+    const server = styleguide.web.server({
       sync: true
     });
-    server.on('error', err => logger.error(err.message));
+    server.on('error', err => Notifier.error(err.message));
     return server.start().then(() => {
-      logger.success(`Fractal server is now running at ${server.url}`);
+      Notifier.log(Notifier.style.green(`Fractal server is now running at ${server.url}`));
     });
 
   });
+
 }
