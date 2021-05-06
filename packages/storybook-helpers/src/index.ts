3dfaab30e5c152bcfdd9cb6078d230a0a20461a5
Tim de Koning <git@king-management.nl>
2021-05-06 23:15:20 +0200

    fix: Incorrect path for storybook-helpers

--- packages/storybook-helpers/src/index.ts
+++ packages/storybook-helpers/src/index.ts
@@ -1,7 +1,7 @@
-import createAttributes from '../scripts/createAttributes';
-import createImage from '../scripts/createImage';
-import DrupalAttribute from '../scripts/drupalAttributes';
-import faker from '../scripts/faker';
+import createAttributes from './scripts/createAttributes';
+import createImage from './scripts/createImage';
+import DrupalAttribute from './scripts/drupalAttributes';
+import faker from './scripts/faker';
 
 export {
   createAttributes, createImage, DrupalAttribute, faker,
